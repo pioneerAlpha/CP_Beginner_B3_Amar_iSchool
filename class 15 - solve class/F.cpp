6b1c2ae7a0b2739a34a509f7ef360fc8efaddd17
tbssajal <45002968+tbssajal@users.noreply.github.com>
2021-01-31 02:31:03 +0600

    Create F.cpp

---
+++ class 15 - solve class/F.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define N ((int)(1e5 + 5))
+#define MAX ((int)2e9 + 5)
+#define MOD ((int)(1e9 + 3))
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL);
+
+
+using namespace std;
+
+// fastio
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int dpp[2005][2005] , tim[2005] , prc[2005] , n ;
+
+int call(int idx , int rem)
+{
+    if(rem <= 0) return 0;
+    if(idx > n){
+        return MAX;
+    }
+    if(dpp[idx][rem] != -1) return dpp[idx][rem];
+    int ans = min(call(idx+1,rem) , call(idx+1,rem-1-tim[idx]) + prc[idx]);
+    return dpp[idx][rem] = ans;
+}
+
+int main()
+{
+    fastio;
+    cin>>n;
+    for(int i = 1 ; i<=n ; i++) cin>>tim[i]>>prc[i];
+    memset(dpp,-1,sizeof dpp);
+    cout<<call(1,n)<<endl;
+    return 0;
+}
