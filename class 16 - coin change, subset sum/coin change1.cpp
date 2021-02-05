4172e389e57cf7042e1b90364c1ca4aef12fd710
tbssajal <45002968+tbssajal@users.noreply.github.com>
2021-02-05 12:05:44 +0600

    Create coin change1.cpp

---
+++ class 16 - coin change, subset sum/coin change1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define N ((int)(1e5 + 5))
+#define MAX ((int)2e9 + 5)
+#define MAXL ((ll)1e18 + 5)
+#define MOD ((int)(1e9 + 3))
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL);
+
+
+using namespace std;
+
+// fastio
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int n , dpp[105][N] , val[105];
+
+/// "yes" "no" variation
+
+int call(int idx , int tot)
+{
+    if(tot < 0) return 0;
+    if(idx > n){
+        if(tot == 0) return 1;
+        return 0;
+    }
+    if(dpp[idx][tot] != -1) return dpp[idx][tot];
+    int ans = 0;
+    ans = call(idx + 1 , tot) | call(idx + 1 , tot - val[idx]);
+    return dpp[idx][tot] = ans;
+}
+
+int main()
+{
+    int tot;
+    cin>>n>>tot;
+    for(int i = 1 ; i<=n ; i++) cin>>val[i];
+    memset(dpp,-1,sizeof dpp);
+    if(call(1,tot)) cout<<"YES\n";
+    else cout<<"NO\n";
+    return 0;
+}
